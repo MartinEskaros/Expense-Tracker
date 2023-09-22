d783a02b63f7d3bc5728484f574347509a5b13f3
MartinEskaros <martineskaros@gmail.com>
2023-09-21 22:14:56 -0400

    Add route route with 'Hello World' response for testing

--- backend/app.js
+++ backend/app.js
@@ -12,3 +12,11 @@ const PORT = process.env.PORT
 //middleWares
 app.use(express.json())
 app.use(cors()) // CORS middleware provides a way to allow or restrict which domains can access your server's resources.
+
+
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+})
+
+
