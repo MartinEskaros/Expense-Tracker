846412f2fe601da498e16c037b6fb4ca26151253
MartinEskaros <martineskaros@gmail.com>
2023-09-22 02:14:06 -0400

    load and mount all routes under '/api/v1' path

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-
+const { db } = require('./db/db');
+const {readdirSync} = require('fs');
+const { route } = require('./routes/transactions');
 const app = express();
 
 
@@ -14,16 +16,22 @@ app.use(express.json())
 app.use(cors()) // CORS middleware provides a way to allow or restrict which domains can access your server's resources.
 
 
+//routes
+const path = require('path');
+readdirSync('./routes').map((route) => app.use('/api/v1', require(path.join(__dirname, 'routes', route))))
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
-})
+
+
+
+
 
 
 const server = () => {
+    db()    //establish connection to MongoDB database
     app.listen(PORT, () => {
         console.log('listening to port:', PORT)
     })
 }
+route
 
 server()
