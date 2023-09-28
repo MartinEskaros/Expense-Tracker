9f6eecbdb566e79dbaee87a6ef9a2671d3d0bdd1
MartinEskaros <martineskaros@gmail.com>
2023-09-27 21:56:27 -0400

    Expand transaction routes and integrate income controllers for getting and deleting incomes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,8 +1,21 @@
+//Routes for financial transactions
+
+
+const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
+
 const router = require('express').Router()  //Seting up the router
 
+//POST route for adding income, calls addIncome function in income.js
+router.post('/add-income', addIncome)
+        .get('/get-incomes',getIncomes)
+        .delete('/delete-income/:id', deleteIncome)
 
-router.get('/transactions', (req,res) => {
-    res.send('Hello World')
-})
 
+
+
+
+
+
+
+//exporting the router to be used in other parts of the application
 module.exports = router;
