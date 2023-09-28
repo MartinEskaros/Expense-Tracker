2002bcd8ec2fe015865d3c4999473fd850c3a109
MartinEskaros <martineskaros@gmail.com>
2023-09-27 23:15:03 -0400

    Add routes for adding,getting and deleting expenses

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,6 +1,7 @@
 //Routes for financial transactions
 
 
+const { getExpenses, deleteExpense, addExpense } = require('../controllers/expense');
 const { addIncome, getIncomes, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router()  //Seting up the router
@@ -9,6 +10,9 @@ const router = require('express').Router()  //Seting up the router
 router.post('/add-income', addIncome)
         .get('/get-incomes',getIncomes)
         .delete('/delete-income/:id', deleteIncome)
+        .post('/add-expense', addExpense)
+        .get('/get-expenses', getExpenses)
+        .delete('/delete-expense/:id', deleteExpense)
 
 
 
