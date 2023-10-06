d95931962ec1b65ab6f94f21b52154099c793aab
MartinEskaros <martineskaros@gmail.com>
2023-10-06 17:32:45 -0400

    Add Icons for menu items

---
+++ expense-tracker-frontend/src/utils/Icons.js
@@ -0,0 +1,4 @@
+export const dashboard = <i className="fa-solid fa-chart-line"></i>
+export const transactions = <i className="fa-solid fa-credit-card"></i>
+export const trend = <i className="fa-solid fa-money-bill-trend-up"></i>
+export const expenses = <i className="fa-solid fa-money-bill-transfer"></i>
