4a69e12920156018ab9a0e6e30e09e16acce93d9
MartinEskaros <martineskaros@gmail.com>
2023-10-07 02:25:04 -0400

    Set Active States for each menu item

--- expense-tracker-frontend/src/App.jsx
+++ expense-tracker-frontend/src/App.jsx
@@ -1,18 +1,44 @@
 import styled from "styled-components";
 import bg from './img/background.jpg';
 import { MainLayout } from "./styles/Layout";
-import Orb from "./Components/Orb/Orb";
+
 import Navigation from "./Components/Navigation/Navigation";
+import React, { useState } from "react";
+
+import Expenses from "./Components/Expenses/Expenses";
+import Income from "./Components/Incomes/Income";
+import Dashboard from "./Components/Dashboard/Dashboard";
 
 
 
 
 function App() {
+  const [active,setActive] = React.useState(1);
+
+  const displayData = ()=>{
+      switch(active){
+        case 1:
+          return <Dashboard/>
+        case 2:
+          return <Dashboard/>
+        case 3:
+          return <Income/>
+        case 4:
+          return <Expenses/>
+        default: 
+          return <Dashboard/>
+      }
+  }
+  
+
   return (
       <AppStyled bg={bg} className='App'>
-        <Orb />
+        
         <MainLayout>
-          <Navigation/>
+          <Navigation active={active} setActive={setActive}/> 
+          <main>
+           {displayData()}
+          </main>
         </MainLayout>
 
       </AppStyled>
@@ -27,6 +53,18 @@ const AppStyled = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    box-shadow: 0px 1px 10px darkblue;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+    }
+  }
 `;
 
 
