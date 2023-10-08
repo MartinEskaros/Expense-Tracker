38338e5336ce065dd1b5593f6355f0a574c52ebf
MartinEskaros <martineskaros@gmail.com>
2023-10-08 01:20:17 -0400

    Add expense functions

--- expense-tracker-frontend/src/context/globalContext.jsx
+++ expense-tracker-frontend/src/context/globalContext.jsx
@@ -37,6 +37,34 @@ export const GlobalProvider = ({children}) => {
         return totalIncome;
     }
 
+    const addExpense = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-expense`, income)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+        getExpenses()
+    }
+
+    const getExpenses = async () => {
+        const response = await axios.get(`${BASE_URL}get-expenses`)
+        setExpenses(response.data)
+        console.log(response.data)
+    }
+
+    const deleteExpense = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-expense/${id}`)
+        getExpenses()
+    }
+
+    const totalExpenses = () => {
+        let totalIncome = 0;
+        expenses.forEach((income) =>{
+            totalIncome = totalIncome + income.amount
+        })
+
+        return totalIncome;
+    }
+
     
 
     return(
@@ -45,7 +73,12 @@ export const GlobalProvider = ({children}) => {
             getIncomes,
             incomes,
             deleteIncome,
-            totalIncome
+            totalIncome,
+            addExpense,
+            getExpenses,
+            expenses,
+            deleteExpense,
+            totalExpenses
         }}>
             {children}
         </GlobalContext.Provider>
