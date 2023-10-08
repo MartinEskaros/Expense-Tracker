18889fc6cfaa8ec29eed071295ce5668ad3a4b70
MartinEskaros <martineskaros@gmail.com>
2023-10-07 23:46:04 -0400

    Add getIncome function to add/delete income to automatically get updated income list

--- expense-tracker-frontend/src/context/globalContext.jsx
+++ expense-tracker-frontend/src/context/globalContext.jsx
@@ -16,11 +16,17 @@ export const GlobalProvider = ({children}) => {
             .catch((err)=>{
                 setError(err.response.data.message)
             })
+            getIncomes();
     }
 
     const getIncomes = async () => {
         const response = await axios.get(`${BASE_URL}get-incomes`)
         setIncomes(response.data);
+    }
+
+    const deleteIncome = async (id) => {
+        const res = await axios.delete(`${BASE_URL}delete-income/${id}`)
+        getIncomes();
     }
 
     
@@ -29,7 +35,8 @@ export const GlobalProvider = ({children}) => {
         <GlobalContext.Provider value = {{
             addIncome,
             getIncomes,
-            incomes
+            incomes,
+            deleteIncome
         }}>
             {children}
         </GlobalContext.Provider>
