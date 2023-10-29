ef692d98718ab3c5be389826ffc0caaba9ec6f33
MartinEskaros <martineskaros@gmail.com>
2023-10-29 01:14:22 -0400

    Added type to income item

--- expense-tracker-frontend/src/Components/Incomes/Income.jsx
+++ expense-tracker-frontend/src/Components/Incomes/Income.jsx
@@ -24,17 +24,18 @@ function Income() {
           </div>
           <div className="incomes">
             {incomes.map((income) => {
-               const {_id,title,amount,date,category,description} = income;
+               const {_id,title,amount,date,category,description,type} = income;
                return <IncomeItem
                                 key={_id}
                                 id={_id} 
                                 title={title} 
                                 description={description} 
                                 amount={amount} 
-                                date={date}                    
+                                date={date}     
+                                type={type}               
                                 category={category} 
                                 indicatorColor="var(--color-green)"
-                                deleteItem={deleteIncome}
+                                deleteItem={deleteIncome}                               
                                 
                             />
             })}
